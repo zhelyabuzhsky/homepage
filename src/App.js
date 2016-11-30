daa1d7af792c25d1e515437154a0fcee3b8cc79c
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2016-11-30 20:23:54 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>
+          <span className="nowrap">Ilya Zhelyabuzhsky</span>
+        </h1>
+        <nav>
+          <h1>Contacts</h1>
+          <ul>
+              <li>
+                  <span className="nowrap">
+                      phone: +7 961 771-5967
+                  </span>
+              </li>
+              <li>
+                  <span className="nowrap">
+                      e-mail: <a href="mailto:zhelyabuzhsky@icloud.com">zhelyabuzhsky@icloud.com</a>
+                  </span>
+              </li>
+              <li>
+                  <span className="nowrap">
+                      facebook: <a href="https://www.facebook.com/zhelyabuzhsky">facebook.com/zhelyabuzhsky</a>
+                  </span>
+              </li>
+              <li>
+                  <span className="nowrap">
+                      twitter: @<a href="https://twitter.com/zhelyabuzhsky">zhelyabuzhsky</a>
+                  </span>
+              </li>
+              <li>
+                  <span className="nowrap">
+                      github: @<a href="https://github.com/zhelyabuzhsky">zhelyabuzhsky</a>
+                  </span>
+              </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
