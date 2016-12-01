912326b898f8a7d79e32474620a210f86b9a422f
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2016-12-01 18:41:38 +0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,38 +6,36 @@ class App extends Component {
     return (
       <div className="container">
         <h1>
-          <span className="nowrap">Ilya Zhelyabuzhsky</span>
+          <span className="text-nowrap">Ilya Zhelyabuzhsky</span>
         </h1>
-        <nav>
-          <h2>Contacts</h2>
-          <ul>
-              <li>
-                  <span className="nowrap">
-                      phone: +7 961 771-5967
-                  </span>
-              </li>
-              <li>
-                  <span className="nowrap">
-                      e-mail: <a href="mailto:zhelyabuzhsky@icloud.com">zhelyabuzhsky@icloud.com</a>
-                  </span>
-              </li>
-              <li>
-                  <span className="nowrap">
-                      facebook: <a href="https://www.facebook.com/zhelyabuzhsky">facebook.com/zhelyabuzhsky</a>
-                  </span>
-              </li>
-              <li>
-                  <span className="nowrap">
-                      twitter: @<a href="https://twitter.com/zhelyabuzhsky">zhelyabuzhsky</a>
-                  </span>
-              </li>
-              <li>
-                  <span className="nowrap">
-                      github: @<a href="https://github.com/zhelyabuzhsky">zhelyabuzhsky</a>
-                  </span>
-              </li>
-          </ul>
-        </nav>
+        <h4>Contacts</h4>
+        <ul>
+          <li>
+            <span className="text-nowrap">
+              phone: +7 961 771-5967
+            </span>
+          </li>
+          <li>
+            <span className="text-nowrap">
+              e-mail: <a href="mailto:zhelyabuzhsky@icloud.com">zhelyabuzhsky@icloud.com</a>
+            </span>
+          </li>
+          <li>
+            <span className="text-nowrap">
+              facebook: <a href="https://www.facebook.com/zhelyabuzhsky">facebook.com/zhelyabuzhsky</a>
+            </span>
+          </li>
+          <li>
+            <span className="text-nowrap">
+              twitter: @<a href="https://twitter.com/zhelyabuzhsky">zhelyabuzhsky</a>
+            </span>
+          </li>
+          <li>
+            <span className="text-nowrap">
+              github: @<a href="https://github.com/zhelyabuzhsky">zhelyabuzhsky</a>
+            </span>
+          </li>
+        </ul>
       </div>
     );
   }
