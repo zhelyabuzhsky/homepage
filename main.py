e10f18dc28831789c49cfd558a7711e75fe3ac08
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-11-03 07:58:22 +0500

    Added 404 handler

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def root():
+def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
